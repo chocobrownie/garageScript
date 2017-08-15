1f54844b669287fa73e31fb350f9cbe7b48e9022
Ming <ming@llip.life>
2017-08-15 15:50:36 -0700

    jsAddTestVersion3

--- jsAddTest/addFunc.js
+++ jsAddTest/addFunc.js
@@ -2,3 +2,4 @@ const addNumbers = (addFirst,addSecond) => {
   return addFirst + addSecond;
 }
 module.exports = addNumbers;
+
--- jsAddTest/testAdd.js
+++ jsAddTest/testAdd.js
@@ -1,12 +1,12 @@
 const addNum = require("./addFunc.js");
-const testFunc = (addOne,addTwo) => {
-  if (addNum(addOne,addTwo) == (addOne+addTwo)){
+const testFunc = (addOne,addTwo,correctAns) => {
+  if (addNum(addOne,addTwo) == correctAns){
     console.log("correct");
   }else{
     console.log("wrong");
   }
 }
-testFunc(1,3);
-testFunc(4,8);
-testFunc(20,38);
-testFunc(2,6);
+testFunc(1,3,4);
+testFunc(4,8,12);
+testFunc(20,38,58);
+testFunc(2,6,8);
