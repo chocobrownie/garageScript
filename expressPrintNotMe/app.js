999eba2ed689c069a8a857f614a1234b5bdc719a
Ming <ming@llip.life>
2017-08-16 12:18:45 -0700

    optimized indentation version 2

--- expressPrintNotMe/app.js
+++ expressPrintNotMe/app.js
@@ -4,14 +4,13 @@ const f = require("fs");
 app.listen(3611);
 app.get("/",(req,res) => {
   let namesNotMe = [];
-  f.readdir("/home",
-    (err,files)=>{
-      files.forEach(oneFile=>{
-        if (oneFile != "ming") {
-          namesNotMe.push(oneFile);
-        }
-      });
-      res.send(namesNotMe);
+  f.readdir("/home", (err,files)=>{
+    files.forEach(oneFile=>{
+      if (oneFile != "ming") {
+        namesNotMe.push(oneFile);
+      }
     });
+    res.send(namesNotMe);
+  });
 });
 
